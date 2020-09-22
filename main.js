7137de503874bd4ae789f4353f7fc504069cd11d
AOskari <71372792+AOskari@users.noreply.github.com>
2020-09-22 12:38:34 +0300

    added comment

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ let locationInformation = [];
 let mymap = L.map('mapid').setView([61.924110, 25.748152], 5);
 
 // Creating the Leaflet map using the Mapbox API and focusing it on Finland
-
+//This is a comment
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
