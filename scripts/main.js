6db01b0d750bf9d6c425a34dcaf964fa63ef8c5e
Markus Nivasalo <markus.nivasalo@outlook.com>
2020-09-27 11:29:40 +0300

    Restructured code completely, moved drawing the ships into its own function, removed a lot of pointless functions

--- scripts/main.js
+++ scripts/main.js
@@ -9,10 +9,6 @@ let weatherIcon = 'http://openweathermap.org/img/wn/';
 let mymap = L.map('mapid').setView([61.924110, 25.748152], 5);
 
 let showAllShips = true;
-let showExactShips = false;
-let showInBetweenShips = false;
-let showBothShips = false;
-let showOtherShips = false;
 
 // Creating the Leaflet map using the Mapbox API and focusing it on Finland
 //This is a comment
@@ -39,6 +35,7 @@ fetch(metadataAPI)
         return answer.json();
     }).then(function(json){
     metadataInformation = json;
+    catchMetadata();
 }).catch(function(error) {
     console.log(error);
 });
@@ -71,26 +68,11 @@ mymap.addEventListener('click', e => {
     })
 });
 
-// HERE ARE ALL THE FUNCTIONS!
-//
-// 1. storeLocationData
-// 2. resetLocationData
-// 3. getWeatherData
-// 4. printWeatherData
-// 5. catchMetadata
-// 6. getShips
-// 7. searchForShip
-// 8. toggle
-// 9. showAll
-// 10. showExact
-// 11. showInBetween
-// 12. showBoth
-// 13. showOthers
-// 14. changeMode
-// 15. removeStartScreen
-// 16. intro
-// 17. displayColors
-//
+/*
+=====================================================
+            HERE ARE ALL THE FUNCTIONS
+=====================================================
+ */
 
 // Store the location data from the API into locationInformation
 
@@ -151,7 +133,7 @@ function printWeatherData(json, latlng) {
 
 // Use the ship metadataInformation and locationInformation collected from the APIs to draw circles representing the ships on the map
 
-function catchMetadata(type, type2, typeName, typeColor) {
+function catchMetadata() {
 
     for (let i = 0; i < metadataInformation.length; i++) {
         // Calling the getShips function to fuse the correct ship location with the correct metadata and to add
@@ -166,98 +148,56 @@ function catchMetadata(type, type2, typeName, typeColor) {
         let shipTypeString;
         let color;
 
-        if (showAllShips) {
-
-            if (shipType === 30) {
-                shipTypeString = 'fishing';
-                color = 'blue';
-            } else if (shipType === 31 || shipType === 32) {
-                shipTypeString = 'towing';
-                color = 'yellow'
-            } else if (shipType === 35) {
-                shipTypeString = 'military';
-                color = 'green';
-            } else if (shipType === 36) {
-                shipTypeString = 'sailing';
-                color = 'white';
-            } else if (shipType === 37) {
-                shipTypeString = 'pleasure craft'
-                color = 'white';
-            } else if (shipType >= 40 && shipType < 50) {
-                shipTypeString = 'high speed craft';
-                color = 'crimson'
-            } else if (shipType === 50) {
-                shipTypeString = 'pilot vessel';
-                color = 'orange'
-            } else if (shipType === 51) {
-                shipTypeString = 'search and rescue';
-                color = 'darkkhaki'
-            } else if (shipType === 52) {
-                shipTypeString = 'tug';
-                color = 'yellow';
-            } else if (shipType === 53) {
-                shipTypeString = 'port tender';
-                color = 'magenta';
-            } else if (shipType === 54) {
-                shipTypeString = 'anti-pollution equipment';
-                color = 'darkslategray'
-            } else if (shipType === 55) {
-                shipTypeString = 'law enforcement';
-                color = 'darkblue';
-            } else if (shipType >= 60 && shipType < 70) {
-                shipTypeString = 'passenger';
-                color = 'red';
-            } else if (shipType >= 70 && shipType < 80) {
-                shipTypeString = 'cargo';
-                color = 'brown';
-            } else if (shipType >= 80 && shipType < 90) {
-                shipTypeString = 'tanker';
-                color = 'black';
-            } else {
-                shipTypeString = 'other';
-                color = 'slategrey';
-            }
-        }
-        if (showExactShips) {
-            if (shipType === type) {
-                shipTypeString = typeName;
-                color = typeColor;
-            } else {
-                continue;
-            }
-        }
-        if (showInBetweenShips) {
-            if (shipType >= +type && shipType < +type2) {
-                shipTypeString = typeName;
-                color = typeColor;
-            } else {
-                continue;
-            }
-        }
-        if (showBothShips) {
-            if (shipType === +type || shipType === +type2 ) {
-                shipTypeString = typeName;
-                color = typeColor;
-            } else {
-                continue;
-            }
-        }
-        if (showOtherShips) {
-            if (shipType < +type || shipType > +type2) {
-                shipTypeString = typeName;
-                color = typeColor;
-            } else {
-                continue;
-            }
+        if (shipType === 30) {
+            shipTypeString = 'fishing';
+            color = 'blue';
+        } else if (shipType === 31 || shipType === 32) {
+            shipTypeString = 'towing';
+            color = 'yellow'
+        } else if (shipType === 35) {
+            shipTypeString = 'military';
+            color = 'green';
+        } else if (shipType === 36) {
+            shipTypeString = 'sailing';
+            color = 'white';
+        } else if (shipType === 37) {
+            shipTypeString = 'pleasure craft'
+            color = 'white';
+        } else if (shipType >= 40 && shipType < 50) {
+            shipTypeString = 'high speed craft';
+            color = 'crimson'
+        } else if (shipType === 50) {
+            shipTypeString = 'pilot vessel';
+            color = 'orange'
+        } else if (shipType === 51) {
+            shipTypeString = 'search and rescue';
+            color = 'darkkhaki'
+        } else if (shipType === 52) {
+            shipTypeString = 'tug';
+            color = 'yellow';
+        } else if (shipType === 53) {
+            shipTypeString = 'port tender';
+            color = 'magenta';
+        } else if (shipType === 54) {
+            shipTypeString = 'anti-pollution equipment';
+            color = 'darkslategray'
+        } else if (shipType === 55) {
+            shipTypeString = 'law enforcement';
+            color = 'darkblue';
+        } else if (shipType >= 60 && shipType < 70) {
+            shipTypeString = 'passenger';
+            color = 'red';
+        } else if (shipType >= 70 && shipType < 80) {
+            shipTypeString = 'cargo';
+            color = 'brown';
+        } else if (shipType >= 80 && shipType < 90) {
+            shipTypeString = 'tanker';
+            color = 'black';
+        } else {
+            shipTypeString = 'other';
+            color = 'slategrey';
         }
 
-        let circle = L.circle([locationInformation[shipMMSI].latitude, locationInformation[shipMMSI].longitude], {
-            id: i,
-            color: color,
-            fillColor: '#f03',
-            fillOpacity: 1.0,
-            radius: 5,
-        }).addTo(ships);
         const name = metadataInformation[i].name;
         let destination = metadataInformation[i].destination;
 
@@ -267,13 +207,11 @@ function catchMetadata(type, type2, typeName, typeColor) {
             destination = destinationArray[1];
         }
 
-        circle.bindPopup(`<p class="popupTextBox">Ship name: ${name}
-                                <br>Ship destination: <a href="https://www.marinetraffic.com/en/ais/index/search/all/keyword:${destination}/search_type:2">${destination}</a>
-                                <br>Ship type: ${shipTypeString}
-                                <br>Ship coordinates: ${locationInformation[shipMMSI].latitude}, ${locationInformation[shipMMSI].longitude}
-                                <br>Weather information: <button onclick="getWeatherData(${locationInformation[shipMMSI].latitude}, ${locationInformation[shipMMSI].longitude})">Click here</button></p>`);
         circleInformation.push({
             'shipMMSI' : locationInformation[shipMMSI].mmsi,
+            'latitude' : locationInformation[shipMMSI].latitude,
+            'longitude' : locationInformation[shipMMSI].longitude,
+            'color' : color,
             'popUp' : `<p class="popupTextBox">Ship name: ${name}
                                 <br>Ship destination: <a href="https://www.marinetraffic.com/en/ais/index/search/all/keyword:${destination}/search_type:2">${destination}</a>
                                 <br>Ship type: ${shipTypeString}
@@ -281,10 +219,32 @@ function catchMetadata(type, type2, typeName, typeColor) {
                                 <br>Weather information: <button onclick="getWeatherData(${locationInformation[shipMMSI].latitude}, ${locationInformation[shipMMSI].longitude})">Click here</button></p>`
 
         });
-
     }
+}
 
-ships.addTo(mymap);
+// Draws the ships on the map
+
+function drawShips(color) {
+    if (showAllShips) {
+        for (let i = 0; i < circleInformation.length; i++) {
+            let circle = L.circle([circleInformation[i].latitude, circleInformation[i].longitude], {
+                color: circleInformation[i].color,
+                radius: 5,
+            }).addTo(ships);
+            circle.bindPopup(circleInformation[i].popUp);
+        }
+    } else {
+        for (let i = 0; i < circleInformation.length; i++) {
+            if (circleInformation[i].color === color) {
+                let circle = L.circle([circleInformation[i].latitude, circleInformation[i].longitude], {
+                    color: circleInformation[i].color,
+                    radius: 5,
+                }).addTo(ships);
+                circle.bindPopup(circleInformation[i].popUp);
+            }
+        }
+    }
+    ships.addTo(mymap);
 }
 
 // Comparing the mmsi information from the location array to the current ship in the metadata list to make sure
@@ -337,95 +297,34 @@ function searchForShip(markers, searchBar) {
 }
 
 function addSearchMarker(i, latitude, longitude) {
-    console.log('Search is happening...');
     marker = L.marker([latitude, longitude]);
     markers.addLayer(marker);
     for (let p = 0; p < circleInformation.length; p++) {
         if (circleInformation[p].shipMMSI === locationInformation[i].mmsi) {
             let circle = L.circle([latitude, longitude], {
-                color: 'black',
-                fillColor: '#f03',
-                fillOpacity: 1.0,
+                color: circleInformation[p].color,
                 radius: 5,
-            }).addTo(ships);
+            }).addTo(mymap);
             circle.bindPopup(circleInformation[p].popUp);
             marker.bindPopup(circleInformation[p].popUp).addTo(mymap).openPopup();
         }
     }
 }
 
-//Show and hide color code elements
-function toggle() {
-    let lista = document.getElementById("colors");
-    //if list contains a hidden class, remove it and add a visible class
-    if (lista.classList.contains("hidden")){
-        lista.classList.remove("hidden");
-        lista.classList.add("visible");
-    } else {
-        //else change the visibility back to hidden
-        lista.classList.remove("visible");
-        lista.classList.add("hidden");
-    }
-}
-
-// Shows all types of ships
+// Clear markers and show all types of ships
 
 function showAll() {
     ships.clearLayers();
     showAllShips = true;
-    showExactShips = false;
-    showInBetweenShips = false;
-    showBothShips = false;
-    showOtherShips = false;
-    catchMetadata();
+    drawShips();
 }
 
-//Clearing markers and displaying ships with precise typenumbers.
+// Clear markers and show specific ships
 
-function showExact(type, type2, typeName, typeColor) {
+function showOther(color) {
     ships.clearLayers();
     showAllShips = false;
-    showExactShips = true;
-    showInBetweenShips = false;
-    showBothShips = false;
-    showOtherShips = false;
-    catchMetadata(type, type2, typeName, typeColor);
-}
-
-//Clearing all markers and filtering shiptypes between numbers.
-
-function showInBetween(type, type2, typeName, typeColor) {
-    ships.clearLayers();
-    showAllShips = false;
-    showExactShips = false;
-    showInBetweenShips = true;
-    showBothShips = false;
-    showOtherShips = false;
-    catchMetadata(type, type2, typeName, typeColor);
-}
-
-//Clearing markers and displaying 2 different shiptypes
-
-function showBoth(type, type2, typeName, typeColor) {
-    ships.clearLayers();
-    showAllShips = false;
-    showExactShips = false;
-    showInBetweenShips = false;
-    showBothShips = true;
-    showOtherShips = false;
-    catchMetadata(type, type2, typeName, typeColor);
-}
-
-//Clearing markers and displaying other shiptypes under shiptype 30 and above 90
-
-function showOthers(type, type2, typeName, typeColor) {
-    ships.clearLayers();
-    showAllShips = false;
-    showExactShips = false;
-    showInBetweenShips = false;
-    showBothShips = false;
-    showOtherShips = true;
-    catchMetadata(type, type2, typeName, typeColor);
+    drawShips(color);
 }
 
 // Handling Dark Mode setting
