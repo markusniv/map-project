63978258ec0b0d9044b93ef0c84a143285ff9ca0
AOskari <oskarar@metropolia.fi>
2020-10-07 12:58:59 +0300

    Optimized Mapbox.

--- scripts/main.js
+++ scripts/main.js
@@ -17,9 +17,6 @@ let showAllShips = true;
 // Creating the Leaflet map using the Mapbox API and focusing it on Finland
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-                 '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
     id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1,
